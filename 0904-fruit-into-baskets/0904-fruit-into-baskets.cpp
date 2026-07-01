4c1cfd28df006058a5c359c1767246db989ad697
Nitin Srinivas <146994721+nitinsrinivas013@users.noreply.github.com>
2026-07-01 09:47:43 +0530

    Time: 42 ms (62.04%), Space: 85 MB (33.94%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -10,7 +10,7 @@ public:
         for(int i=0; i<n; i++){
             mpp[fruits[i]]++;
 
-            if(mpp.size() > 2){
+            while(mpp.size() > 2){
                 mpp[fruits[j]]--;
                 if(mpp[fruits[j]] == 0){
                     mpp.erase(fruits[j]);
