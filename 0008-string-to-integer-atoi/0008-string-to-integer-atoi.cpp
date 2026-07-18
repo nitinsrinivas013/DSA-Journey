431b27f054942efd1d1dcb5e06c22cfc8718df56
Nitin Srinivas <146994721+nitinsrinivas013@users.noreply.github.com>
2026-07-18 11:30:22 +0530

    Time: 3 ms (13.78%), Space: 10.2 MB (5.51%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,60 +1,57 @@
 class Solution {
 public:
 
-    void helper(int index, string s, long long &number, bool &isNegative){
+    void helper(string s, int index, long long &number, bool &isNegative){
 
-        if(index == s.length() || !isdigit(s[index])){
+        if(index > s.length() || !isdigit(s[index])){
             return;
         }
         int digit = s[index] - '0';
 
         if(!isNegative){
-            if (number > INT_MAX / 10 ||
-                (number == INT_MAX / 10 && digit > 7)
-                ) 
-                {
-                    number = INT_MAX;
-                    return;
-                }    
+            if(number > INT_MAX/10 || (number == INT_MAX/10 && digit > 7)){
+                number = INT_MAX;
+                return;
+            }
         }
         else{
-            if (number > (long long)INT_MAX / 10 ||
-                (number == (long long)INT_MAX / 10 && digit > 8)
-                ) 
-                {
-                    number = (long long)INT_MAX + 1;
-                    return;
-                }    
+            if(number > INT_MAX/10 || (number == INT_MAX/10 && digit > 8)){
+                number = (long long) INT_MAX + 1;
+                return;
+            }
         }
 
         number = number * 10 + digit;
-        helper(index + 1, s, number, isNegative);
+        helper(s, index+1, number, isNegative);
     }
+
 
     int myAtoi(string s) {
 
-
-
-        long long number = 0;
-        bool isNegative = false;
         int index = 0;
+        long long number = 0;
+        int n = s.length();
+        bool isNegative = false;
 
-        while(index < s.length() && isspace(s[index])){
+        while(index < n && isspace(s[index])){
             index++;
         }
-        if(index < s.length() && (s[index] == '-' || s[index] == '+')){
+        if(index < n && (s[index] == '+' || s[index] == '-')){
+
             if(s[index] == '-'){
                 isNegative = true;
             }
             index++;
         }
 
-        helper(index, s, number, isNegative);
+        helper(s,index,number,isNegative);
 
         if(isNegative){
-            number = -number;
+            number = -(number);
+            return (int)number;
         }
+
+        return number;
         
-        return (int)number;
     }
 };
