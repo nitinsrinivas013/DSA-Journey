f24aa26dafe0dfbcc55ab87770dd753c51c97ccb
Nitin Srinivas <146994721+nitinsrinivas013@users.noreply.github.com>
2026-07-18 12:28:53 +0530

    Time: 0 ms (100%), Space: 8.6 MB (70.61%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long power = n;
+        double answer = 1;
+
+        if(power < 0){
+            power = -(power);
+        }
+        while(power > 0){
+            if(power%2 != 0){
+                answer = answer * x;
+                power--;
+            }
+            else{
+                x = x*x;
+                power = power / 2;
+            }
+        }
+        if(n < 0){
+            answer = 1/answer;
+        }
+        return answer;
+    }
+};
