2a78cdba2c93e916f3b32a253508dfd7b53c6914
Nitin Srinivas <146994721+nitinsrinivas013@users.noreply.github.com>
2026-07-22 22:15:17 +0530

    Time: 3 ms (40.23%), Space: 16.2 MB (52.57%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,44 +11,42 @@
  */
 class Solution {
 public:
-
-    void dfsTraversal
-    (
-        TreeNode* root,  
-        map <int, map <int, multiset<int>>>& mpp,
-        int x,
-        int y
-    ){
-        if(root == nullptr){
-            return;
-        }
-        dfsTraversal(root -> left, mpp, x - 1, y + 1);
-        mpp[x][y].insert(root -> val);
-        dfsTraversal(root -> right, mpp, x + 1, y + 1);
-    }
-
     vector<vector<int>> verticalTraversal(TreeNode* root) {
 
         vector<vector<int>> result;
-
-        map <int, map <int, multiset<int>>> mpp;
-        int x = 0;
-        int y = 0;
 
         if(root == nullptr){
             return result;
         }
 
-        dfsTraversal(root,mpp,x,y);
+        queue <pair <TreeNode*, pair<int,int>>> q;
+        map <int, map <int, multiset<int>>> mpp;
+        q.push({root, {0,0}});
 
-        for(auto it: mpp){
-            vector <int> levels;
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                auto element = q.front();
+                q.pop();
+                TreeNode* node = element.first;
+                int x = element.second.first;
+                int y = element.second.second;
+
+                if(node -> left){
+                    q.push({node -> left, {x-1, y+1}});
+                }
+                if(node -> right){
+                    q.push({node -> right, {x+1, y+1}});
+                }
+                mpp[x][y].insert(node -> val);
+            }
+        }
+        for(auto it : mpp){
+            vector<int> levels;
             for(auto level : it.second){
-
-                for(int val: level.second){
+                for(int val : level.second){
                     levels.push_back(val);
                 }
-
             }
             result.push_back(levels);
         }
