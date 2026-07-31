01f601752942bad8d78f0b4646b3cd9bf713ef52
Nitin Srinivas <146994721+nitinsrinivas013@users.noreply.github.com>
2026-07-31 09:52:51 +0530

    Time: 0 ms (100%), Space: 8.5 MB (23.87%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
 
-    int memoizationCode(int n, vector<int> &dp){
+    int tabulationCode(int n, vector<int> &dp){
 
-        if(n == 0){
-            dp[n] = 0;
-            return 0;
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
         }
-        if(n == 1 || n == 2){
-            dp[n] = 1;
-            return 1;
-        }
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        dp[n] = memoizationCode(n-3, dp) + memoizationCode(n-2, dp) + memoizationCode(n-1, dp);
         return dp[n];
     }
 
     int tribonacci(int n) {
+
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1 || n == 2){
+            return 1;
+        }
         
         vector<int> dp(n+1,-1);
-        return memoizationCode(n, dp);
+        return tabulationCode(n, dp);
     }
 };
