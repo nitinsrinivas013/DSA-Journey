3a8c8295f366bdd91713a141d9fcd35490a10234
Nitin Srinivas <146994721+nitinsrinivas013@users.noreply.github.com>
2026-08-31 15:09:10 +0530

    Time: 49 ms (48.45%), Space: 29.1 MB (73.16%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -8,7 +8,7 @@ public:
 
         int n = nums.size();
 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n-2; i++){
 
             if(i > 0 && nums[i] == nums[i-1]){
                 continue;
